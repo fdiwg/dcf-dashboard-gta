88d34c78d5d4715a6e8b1ae12a4ce34e4a2843f3
Bennici <alexandre-ben@hotmail.fr>
2025-04-01 09:17:04 +0200

    allow download data option

--- server.R
+++ server.R
@@ -4,12 +4,33 @@ server <- function(input, output,session) {
   #---------------------------------------------------------------------------------------
   dcf.dashboard::dcf_dashboard_server(
     input = input, output = output, session = session,
-    tasks = c(
-      "Task I: Nominal catches"="task_i",
-      "Task II: Catches"="task_ii"
-    ),
+    tasks =  list(
+      list(
+        id = "task_i",
+        label = "Task I: Nominal catches",
+        formats = list(
+          list(
+            id = "generic",
+            label="Generic",
+            ref= "https://raw.githubusercontent.com/fdiwg/fdi-formats/main/cwp_rh_generic_gta_taskI.json"
+          )
+        )
+      ),
+        list(
+          id = "task_ii",
+          label = "Task II: Catches",
+          formats = list(
+            list(
+              id = "generic",
+              label="Generic",
+              ref= "https://raw.githubusercontent.com/fdiwg/fdi-formats/main/cwp_rh_generic_gta_taskII.json"
+            )
+          )
+        )
+      ),
     reporting_entity = c("RFMOs" = "source_authority"),
-    ref_reporting_entities = "https://raw.githubusercontent.com/fdiwg/fdi-codelists/main/global/firms/gta/cl_tuna_rfmos.csv"
+    ref_reporting_entities = "https://raw.githubusercontent.com/fdiwg/fdi-codelists/main/global/firms/gta/cl_tuna_rfmos.csv",
+    allow_download=TRUE
   )
 
 }
